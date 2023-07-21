5ba8568b31cc2ab58db0a73a877f57eb4b2dbe74
dhanusree2001 <139853283+dhanusree2001@users.noreply.github.com>
2023-07-21 12:55:27 +0530

    Update python.py

--- python.py
+++ python.py
@@ -2,4 +2,5 @@ this is my second file in git I am going to push to remote repo
 i am in development branch
 this is in master branch
 this for conflicts in dev branch
+this is done directly in git 
 
